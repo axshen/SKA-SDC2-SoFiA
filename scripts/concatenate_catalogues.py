45db8507f842493e950ff9c9e87f5f12c69b6eec
SoFiA-Admin <tobias.westmeier@uwa.edu.au>
2021-08-13 14:36:11 +0800

    Added catalogue concatenation script

---
+++ scripts/concatenate_catalogues.py
@@ -0,0 +1,14 @@
+from astropy.io.votable import parse_single_table, from_table, writeto
+from astropy.table import Table, vstack
+
+print("Loading catalogue 1");
+table = parse_single_table("catalogues/sofia_sdc2_cat.xml").to_table();
+
+for i in range(2, 81):
+	print("Loading catalogue {:d}".format(i));
+	table2 = parse_single_table("catalogues/sofia_sdc2_{:03d}_cat.xml".format(i)).to_table();
+	table = vstack([table, table2]);
+
+print("Writing merged catalogue");
+votable = from_table(table);
+writeto(votable, "merged_catalogue.xml");
