9b309fd7b9cd39b8438ec108117c20626ea6aebe
SoFiA <tobias.westmeier@uwa.edu.au>
2021-08-30 12:33:29 +0800

    Update concatenate_catalogues.py

--- scripts/concatenate_catalogues.py
+++ scripts/concatenate_catalogues.py
@@ -2,11 +2,11 @@ from astropy.io.votable import parse_single_table, from_table, writeto
 from astropy.table import Table, vstack
 
 print("Loading catalogue 1");
-table = parse_single_table("catalogues/sofia_sdc2_001_cat.xml").to_table();
+table = parse_single_table("sofia_sdc2_001_cat.xml").to_table();
 
 for i in range(2, 81):
 	print("Loading catalogue {:d}".format(i));
-	table2 = parse_single_table("catalogues/sofia_sdc2_{:03d}_cat.xml".format(i)).to_table();
+	table2 = parse_single_table("sofia_sdc2_{:03d}_cat.xml".format(i)).to_table();
 	table = vstack([table, table2]);
 
 print("Writing merged catalogue");
